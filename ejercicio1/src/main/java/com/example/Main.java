f230ec01be3eab2879f527d7580ef04ff6a8bccf
Pipe9780 <casa2303@gmail.com>
2024-04-17 08:30:40 -0500

    ejercicio1

--- ejercicio1/src/main/java/com/example/Main.java
+++ ejercicio1/src/main/java/com/example/Main.java
@@ -1,7 +1,20 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ingrese un numero entero, el programa decidira si es un numero positivo, negativo o 0");
+        int entero = scanner.nextInt();
+
+        if (entero >= 1){
+            System.out.println("el numero ingresado es positivo");
+        } else if (entero < 0){
+            System.out.println("el numero ingresado es negativo");
+        } else {
+        System.out.println("el numero ingresado es 0");
+         }
     }
+    
 }
