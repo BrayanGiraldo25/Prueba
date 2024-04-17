6826e6650e9b915b4dd95eee7f5faed08e02e2c0
Pipe9780 <casa2303@gmail.com>
2024-04-17 09:07:45 -0500

    ejercicio5

--- ejercicio5/src/main/java/com/example/Main.java
+++ ejercicio5/src/main/java/com/example/Main.java
@@ -2,6 +2,18 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int temperatura =  5;
+        double celsius;
+        double farenheirt;
+
+        if (temperatura < 0){
+            celsius = 5 * 5 /9 + 32;
+            farenheirt = celsius;
+            System.out.println("La temperatura Farenheirt es de "+farenheirt);
+        } else if(temperatura > 0){
+            farenheirt = 50 - 32 * 5 / 9;
+            celsius = farenheirt;
+            System.out.println("la temperatura en celsisu es de "+celsius);
+        }
     }
 }
