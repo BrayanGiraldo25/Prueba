ab5b8d8a6841cac8b538c881a97d02d167208af8
Pipe9780 <casa2303@gmail.com>
2024-04-17 08:54:26 -0500

    ejercicio3

--- ejercicio3/src/main/java/com/example/Main.java
+++ ejercicio3/src/main/java/com/example/Main.java
@@ -1,7 +1,14 @@
 package com.example;
 
+// Brayan Estiben Giraldo Garcia
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int numero = 8;
+       if (numero % 2 == 0){
+        System.out.println("el numero es Par");
+       } else {
+        System.out.println("El numero es impar");
+       }
     }
 }
