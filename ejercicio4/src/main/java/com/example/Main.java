764cd7eb125cdae5ad164b6fc2da089306fea024
Pipe9780 <casa2303@gmail.com>
2024-04-17 08:59:37 -0500

    ejercicio4

--- ejercicio4/src/main/java/com/example/Main.java
+++ ejercicio4/src/main/java/com/example/Main.java
@@ -2,6 +2,14 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Ejercicio 4: Area de un circulo mayor a 100");
+        int RadioCirculo = 35;
+        double AreaCirculo = RadioCirculo * Math.PI;
+        System.out.print(AreaCirculo);
+        if (AreaCirculo > 100){
+            System.out.println(" El area del circulo es mayor a 100");
+        } else {
+            System.out.println("el area del circulo es menor a 100");
+        }
     }
 }
